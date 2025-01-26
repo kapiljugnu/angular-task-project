8e3a9cd23514839ca8b7f29ac3e9e59dfde57fd3
Kapil Jugnu <kr_kapil@hotmail.com>
2025-01-26 13:48:35 +0530

    interface

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,16 +1,21 @@
 import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
+interface User { 
+  id: string; 
+  name: string; 
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({required: true}) user!: { id:string; name:string; avatar: string;};
-  @Output() select = new EventEmitter<string>()
-
+  @Input({ required: true }) user!: User;
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
