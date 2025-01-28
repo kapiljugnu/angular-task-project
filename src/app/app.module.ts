093ff4b93fbf08f49a4e117cbff4250d13b13b68
Kapil Jugnu <kr_kapil@hotmail.com>
2025-01-28 18:08:39 +0530

    shared and task module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { CardComponent } from "./shared/card/card.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { TaskComponent } from "./tasks/task/task.component";
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, UserComponent, CardComponent, TasksComponent, NewTaskComponent, TaskComponent],
+    declarations: [AppComponent, HeaderComponent, UserComponent],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule]
+    imports: [BrowserModule, SharedModule, TasksModule]
 })
 export class AppModule {
 
