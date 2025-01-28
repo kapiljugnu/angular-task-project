5e826f599b3c08095c19fbd9801535afe08f91c1
Kapil Jugnu <kr_kapil@hotmail.com>
2025-01-28 18:00:10 +0530

    angular module

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,6 @@ import { Component } from "@angular/core";
 // define the metadata for the component
 @Component({
     selector: 'app-header', // define tag with 2 words otherwise it will clash with builtin element
-    standalone: true,
-    // template: '<h1>Hello World</h1>'
     templateUrl: './header.component.html',
     styleUrl: './header.component.css'
 })
--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,11 +1,8 @@
 import { Component, EventEmitter, Output } from "@angular/core";
-import { FormsModule } from "@angular/forms";
 import { NewTask } from "../task/task.model";
 
 @Component({
     selector: 'app-new-task',
-    standalone: true,
-    imports: [FormsModule],
     templateUrl: './new-task.component.html',
     styleUrl: './new-task.component.css'
 })
